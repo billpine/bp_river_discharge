c8e96ddfdf3111bc1162381cc973e5a712359176
Bill <billpine@ufl.edu>
2021-02-25 15:47:58 -0500

    updating to include the number of days more than 10% below period of record

--- 20210225_flow_duration_curves_Apalachicola.R
+++ 20210225_flow_duration_curves_Apalachicola.R
@@ -63,25 +63,25 @@ disE  = dis[dis$dates>='1930-10-01' & dis$dates<='2020-12-31',]
 par(mfcol=c(1,2))
 
 #subset for years of interest
-dis.fdc = as.data.frame(cbind(ts(disE$val[disE$year<2021]),ts(disE$val[disE$year>2007&disE$year<2013]),
+dis.fdc = as.data.frame(cbind(ts(disE$val[disE$year<2021]),ts(disE$val[disE$year>2004&disE$year<2013]),
                               ts(disE$val[disE$year > 2012])))
 
 
 #create myself (not using fdc() function)
 all <- sort(dis.fdc$`ts(disE$val[disE$year < 2021])`, decreasing = T)
-part <- sort(dis.fdc$`ts(disE$val[disE$year > 2007 & disE$year < 2013])`,decreasing = T)
+part <- sort(dis.fdc$`ts(disE$val[disE$year > 2004 & disE$year < 2013])`,decreasing = T)
 part2 <- sort(dis.fdc$`ts(disE$val[disE$year > 2012])`, decreasing = T)
 
 df_all <- data.frame(x = 100/length(all) * 1:length(all), y = all)
-df_part_2008_2012 <- data.frame(x = 100/length(part) * 1:length(part), y = part)
+df_part_2005_2012 <- data.frame(x = 100/length(part) * 1:length(part), y = part)
 df_part_2013_2020 <- data.frame(x = 100/length(part2) * 1:length(part2), y = part2)
 
 #make plots
 plot(x = df_all$x, y = log(df_all$y), type = 'l', lwd = 2, ylab = "Log(Flow (cf/s))", 
      xlab = "% Time flow equalled or exceeded", xaxt = 'n',main='Discharge duration')
-lines(x = df_part_2008_2012$x, y = log(df_part_2008_2012$y), type = 'l', col = 'red', lwd = 2)
+lines(x = df_part_2005_2012$x, y = log(df_part_2005_2012$y), type = 'l', col = 'red', lwd = 2)
 abline(h = log(5000), lwd = 2, col = 'blue', lty = 2)
-legend("topright", legend = c("1930-2020", "2008-2012"), lty = 1, col = c("black", "red"))
+legend("topright", legend = c("1930-2020", "2005-2012"), lty = 1, col = c("black", "red"))
 axis(1, labels = c("0%", "20%", "40%", "60%", "80%", "100%"), at = c(0, 20, 40, 60,80, 100))
 
 plot(x = df_all$x, y = log(df_all$y), type = 'l', lwd = 2, ylab = "Log(Flow (cf/s))", 
@@ -99,16 +99,16 @@ df_all2 <- df_all
 df_all2$x <- round(df_all2$x, digits = 2)
 df_all3 <- aggregate(df_all2, by = list(df_all2$x), FUN = 'mean')
 
-df_part_2008_2012_2 <- df_part_2008_2012
-df_part_2008_2012_2$x <- round(df_part_2008_2012_2$x, digits = 2)
-df_part_2008_2012_3 <- aggregate(df_part_2008_2012_2, by = list(df_part_2008_2012_2$x), FUN = 'mean')
+df_part_2005_2012_2 <- df_part_2005_2012
+df_part_2005_2012_2$x <- round(df_part_2005_2012_2$x, digits = 2)
+df_part_2005_2012_3 <- aggregate(df_part_2005_2012_2, by = list(df_part_2005_2012_2$x), FUN = 'mean')
 
-df_merge  <- merge(df_part_2008_2012_3, df_all3, by.x = "x", by.y = "x")
+df_merge  <- merge(df_part_2005_2012_3, df_all3, by.x = "x", by.y = "x")
 
 length(which(log(df_merge$y.x) < log(df_merge$y.y)))
 length(df_merge$y.x)
 
-#0.955 (1744/1927) #this depends on which gauge and years used
+#0.965 (2820/2922) #this depends on which gauge and years used
 
 #how often is the 2013-2020 line below the period of record line
 df_part_2013_2020_2 <- df_part_2013_2020
@@ -117,7 +117,26 @@ df_part_2013_2020_3 <- aggregate(df_part_2013_2020_2, by = list(df_part_2013_202
 
 df_merge2  <- merge(df_part_2013_2020_3, df_all3, by.x = "x", by.y = "x")
 
+
 length(which(log(df_merge2$y.x) < log(df_merge2$y.y)))
 length(df_merge2$y.x)
 
 #1422/2922 = 48.67%
+
+
+
+########
+## how often is the current values more than 10% below the average?
+
+
+#how often 2005-2012 is below?
+df_merge$y.y10 <- df_merge$y.y * 0.90
+length(which(log(df_merge$y.x) < log(df_merge$y.y10)))#2687
+length(df_merge$y.x)#2922
+#2687/2922 = 92%
+
+#how often 2013-2020 below?
+df_merge2$y.y10 <- df_merge2$y.y * 0.90
+length(which(log(df_merge2$y.x) < log(df_merge2$y.y10))) #0
+length(df_merge2$y.x)#2922
+#0/2922 = 0%
